c3a8eed64f5a9a01d86371ad021f6e725d762b0f
Dave Syer <dsyer@vmware.com>
2024-09-05 13:12:34 +0100

    Add server sample

---
+++ samples/grpc-server/src/test/java/org/springframework/grpc_server/GrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.grpc_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
