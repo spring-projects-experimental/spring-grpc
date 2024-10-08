b30dc45e5a48a9e5dd42dd9dbc3c796e7353f438
Dave Syer <dsyer@vmware.com>
2024-10-08 14:33:48 +0100

    Add README to sample and make sure it works

--- samples/grpc-server/src/main/java/org/springframework/grpc/sample/GrpcServerApplication.java
+++ samples/grpc-server/src/main/java/org/springframework/grpc/sample/GrpcServerApplication.java
@@ -2,12 +2,21 @@ package org.springframework.grpc.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.grpc.BindableService;
+import io.grpc.protobuf.services.ProtoReflectionService;
 
 @SpringBootApplication
 public class GrpcServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrpcServerApplication.class, args);
+	}
+
+	@Bean
+	public BindableService serverReflection() {
+		return ProtoReflectionService.newInstance();
 	}
 
 }
