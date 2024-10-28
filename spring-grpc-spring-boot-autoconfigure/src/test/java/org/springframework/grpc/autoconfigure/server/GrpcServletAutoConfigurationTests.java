7da1e80b55825c14ef6e4fd6a502f6643b214f34
Toshiaki Maki <makingx@gmail.com>
2024-10-28 21:44:45 +0900

    Allow GrpcServlet to be configured
    This adds the ability to apply `ServerBuilderCustomizer` customizers
    and `spring.grpc.server.max-inbound-message-size` property to
    `GrpcServlet`.
    
    Signed-off-by: Toshiaki Maki <makingx@gmail.com>

--- spring-grpc-spring-boot-autoconfigure/src/test/java/org/springframework/grpc/autoconfigure/server/GrpcServletAutoConfigurationTests.java
+++ spring-grpc-spring-boot-autoconfigure/src/test/java/org/springframework/grpc/autoconfigure/server/GrpcServletAutoConfigurationTests.java
@@ -16,49 +16,31 @@
 
 package org.springframework.grpc.autoconfigure.server;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.time.Duration;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import org.assertj.core.api.InstanceOfAssertFactories;
+import io.grpc.BindableService;
+import io.grpc.ServerServiceDefinition;
+import io.grpc.internal.GrpcUtil;
+import io.grpc.servlet.jakarta.ServletServerBuilder;
 import org.junit.jupiter.api.Test;
-import org.mockito.InOrder;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-import org.mockito.stubbing.Answer;
+
 import org.springframework.boot.autoconfigure.AutoConfigurations;
-import org.springframework.boot.autoconfigure.ssl.SslAutoConfiguration;
 import org.springframework.boot.test.context.FilteredClassLoader;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.grpc.server.GrpcServerFactory;
-import org.springframework.grpc.server.NettyGrpcServerFactory;
 import org.springframework.grpc.server.ServerBuilderCustomizer;
-import org.springframework.grpc.server.ShadedNettyGrpcServerFactory;
-import org.springframework.grpc.server.lifecycle.GrpcServerLifecycle;
+import org.springframework.util.unit.DataSize;
 
-import io.grpc.BindableService;
-import io.grpc.Grpc;
-import io.grpc.ServerBuilder;
-import io.grpc.ServerServiceDefinition;
-import io.grpc.ServiceDescriptor;
-import io.grpc.netty.NettyServerBuilder;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Tests for {@link GrpcServerAutoConfiguration}.
  *
  * @author Chris Bono
+ * @author Toshiaki Maki
  */
 class GrpcServletAutoConfigurationTests {
 
@@ -91,6 +73,31 @@ class GrpcServletAutoConfigurationTests {
 	@Test
 	void whenWebApplicationServletIsAutoConfigured() {
 		this.contextRunner().run((context) -> assertThat(context).getBean(ServletRegistrationBean.class).isNotNull());
+	}
+
+	@Test
+	void whenCustomizerIsRegistered() {
+		ServerBuilderCustomizer<ServletServerBuilder> customizer = mock();
+		this.contextRunner()
+			.withBean(ServerBuilderCustomizer.class, () -> customizer)
+			.run(context -> verify(customizer).customize(any(ServletServerBuilder.class)));
+	}
+
+	@Test
+	void whenMaxInboundMessageSizeIsSetThenItIsUsed() {
+		this.contextRunner()
+			.withPropertyValues("spring.grpc.server.max-inbound-message-size=10KB")
+			.run(context -> assertThat(context).getBean(ServletRegistrationBean.class)
+				.hasFieldOrPropertyWithValue("servlet.servletAdapter.maxInboundMessageSize",
+						Math.toIntExact(DataSize.ofKilobytes(10).toBytes())));
+	}
+
+	@Test
+	void whenMaxInboundMessageSizeIsNotSetThenDefaultIsUsed() {
+		this.contextRunner()
+			.run((context) -> assertThat(context).getBean(ServletRegistrationBean.class)
+				.hasFieldOrPropertyWithValue("servlet.servletAdapter.maxInboundMessageSize",
+						GrpcUtil.DEFAULT_MAX_MESSAGE_SIZE));
 	}
 
 }
