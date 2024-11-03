7160f274eec0bba971abd56923146abcd4dcff82
CyberZujo <hara_zujo@hotmail.com>
2024-11-03 12:17:41 +0100

    https://github.com/spring-projects-experimental/spring-grpc/issues/26 - Add autoconfig for reflection service and friends #26
    Signed-off-by: CyberZujo <hara_zujo@hotmail.com>

---
+++ spring-grpc-spring-boot-autoconfigure/src/main/java/org/springframework/grpc/autoconfigure/server/GrpcServerReflectionAutoConfiguration.java
@@ -0,0 +1,30 @@
+package org.springframework.grpc.autoconfigure.server;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.grpc.BindableService;
+import io.grpc.protobuf.services.ProtoReflectionService;
+
+/**
+ * {@link EnableAutoConfiguration Auto-configuration} for gRPC Reflection service
+ * <p>
+ * grpc.reflection.enabled=true must be present in configuration in order for the
+ * auto-configuration to execute
+ *
+ * @author Haris Zujo
+ */
+@Configuration
+@ConditionalOnClass(BindableService.class)
+public class GrpcServerReflectionAutoConfiguration {
+
+	@Bean
+	@ConditionalOnProperty(name = "grpc.server.reflection.enabled", havingValue = "true")
+	public BindableService serverReflection() {
+		return ProtoReflectionService.newInstance();
+	}
+
+}
---
+++ spring-grpc-spring-boot-autoconfigure/src/test/java/org/springframework/grpc/autoconfigure/server/GrpcServerReflectionAutoConfigurationTests.java
@@ -0,0 +1,34 @@
+package org.springframework.grpc.autoconfigure.server;
+
+import io.grpc.BindableService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.grpc.server.lifecycle.GrpcServerLifecycle;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GrpcServerReflectionAutoConfigurationTests {
+
+	private ApplicationContextRunner contextRunner() {
+		return new ApplicationContextRunner()
+			.withConfiguration(AutoConfigurations.of(GrpcServerReflectionAutoConfiguration.class))
+			.withBean("noopServerLifecylcle", GrpcServerLifecycle.class, Mockito::mock);
+	}
+
+	@Test
+	void whenReflectionEnabledThenCreateBean() {
+		this.contextRunner()
+			.withPropertyValues("grpc.server.reflection.enabled=true")
+			.run((context) -> assertThat(context).hasSingleBean(BindableService.class));
+	}
+
+	@Test
+	void whenReflectionDisabledThenSkipBeanCreation() {
+		this.contextRunner()
+			.withPropertyValues("grpc.server.reflection.enabled=false")
+			.run((context) -> assertThat(context).doesNotHaveBean(BindableService.class));
+	}
+
+}
