d61af1a1a20825c5d8c84db29705ce9883dd106c
CyberZujo <hara_zujo@hotmail.com>
2024-11-03 12:17:41 +0100

    Add autoconfig for reflection service and friends #26
    Signed-off-by: CyberZujo <hara_zujo@hotmail.com>

---
+++ spring-grpc-spring-boot-autoconfigure/src/main/java/org/springframework/grpc/autoconfigure/server/GrpcServerReflectionAutoConfiguration.java
@@ -0,0 +1,30 @@
+package org.springframework.grpc.autoconfigure.server;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+import io.grpc.BindableService;
+import io.grpc.protobuf.services.ProtoReflectionService;
+
+/**
+ * {@link EnableAutoConfiguration Auto-configuration} for gRPC Reflection service
+ * <p>
+ * This auto-configuration is enabled by default. To disable it, set the configuration
+ * flag {spring.grpc.server.reflection.enabled=false} in your application properties.
+ *
+ * @author Haris Zujo
+ */
+@AutoConfiguration(before = GrpcServerFactoryAutoConfiguration.class)
+@ConditionalOnClass(ProtoReflectionService.class)
+@ConditionalOnProperty(name = "spring.grpc.server.reflection.enabled", havingValue = "true", matchIfMissing = true)
+public class GrpcServerReflectionAutoConfiguration {
+
+	@Bean
+	public BindableService serverReflection() {
+		return ProtoReflectionService.newInstance();
+	}
+
+}
