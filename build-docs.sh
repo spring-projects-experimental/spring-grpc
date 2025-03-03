b88b8fcbd07532c58b9a4a7c9857d70891a58d4b
Chris Bono <chris.bono@gmail.com>
2025-03-03 15:38:52 -0600

    Fix docs.yml workflow (add 'package' command)
    The recent move to do config props generation only at docs
    generation time left the docs.yml broken as it requires the
    config props generator class to be compiled prior to running.
    
    This updates the './build-docs.sh' to include the package command
    as well as update the docs.yml to use the same script.
    
    Signed-off-by: Chris Bono <chris.bono@gmail.com>

--- build-docs.sh
+++ build-docs.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Be sure the ConfigurationPropertiesAsciidocGenerator is compiled
+./mvnw -pl spring-grpc-docs package
+
+# Generate the config props and antora site
 ./mvnw -pl spring-grpc-docs process-resources antora -P docs
